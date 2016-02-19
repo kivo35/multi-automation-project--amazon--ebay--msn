70bb493868a5a5699076d6d994c4c9f0664f2227
Kirill <kivo35@hotmail.com>
2016-02-18 19:53:21 -0800

    Create MSN test class

---
+++ src/test/java/com/sqa/auto/MSN.java
@@ -0,0 +1,19 @@
+package com.sqa.auto;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class MSN {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
