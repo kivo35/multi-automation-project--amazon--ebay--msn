e6117df69c891b0c670cd8d7284ccc8dd1300f6a
Kirill <kivo35@hotmail.com>
2016-02-18 19:52:41 -0800

    Create Ebay test class

---
+++ src/test/java/com/sqa/auto/Ebay.java
@@ -0,0 +1,19 @@
+package com.sqa.auto;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Ebay {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
