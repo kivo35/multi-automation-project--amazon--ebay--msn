0a127d15d236d87a0e63393074dea7864d342dfa
Chance Shiflett <chanceshiflett@yahoo.com>
2016-02-18 21:31:47 -0800

    Add cartQuantity test

--- src/test/java/com/sqa/auto/Amazon.java
+++ src/test/java/com/sqa/auto/Amazon.java
@@ -1,19 +1,54 @@
 package com.sqa.auto;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class Amazon {
-  @Test
-  public void f() {
-  }
-  @BeforeClass
-  public void beforeClass() {
-  }
 
-  @AfterClass
-  public void afterClass() {
-  }
+	private WebDriver driver;
 
+	private String baseUrl;
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		baseUrl = "http://wwww.amazon.com/";
+	}
+
+	@Test(dataProvider = "quantityData")
+	public void cartQuantity(String label, String item, String quantity) {
+		driver.get(baseUrl);
+		System.out.println(label);
+		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys(item);
+		driver.findElement(By.cssSelector(".nav-input")).click();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath(".//*[@id='result_1']/div/div/div/div[2]/div[1]/a/h2")).click();
+		driver.findElement(By.cssSelector("#quantity")).sendKeys(quantity);
+		driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+		WebElement cartNum = driver.findElement(By.id("nav-cart-count"));
+		System.out.println(cartNum.getText());
+		Assert.assertEquals(cartNum.getText(), quantity);
+	}
+	// @Test
+	// public void f() {
+	// }
+
+	@DataProvider
+	public Object[][] quantityData() {
+		return new Object[][] { new Object[] { "Test 1", "Apple Watch", "3" } };
+	}
 }
